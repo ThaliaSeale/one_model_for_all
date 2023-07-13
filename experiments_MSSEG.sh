8aeca027df8c80299fb504379d0830fa6f9d2dac
Thalia Seale <thalia@engs-29584.eng.ox.ac.uk>
2023-07-13 15:57:10 +0100

    experiments_files

---
+++ experiments_MSSEG.sh
@@ -0,0 +1,13 @@
+run_experiment () {
+    mkdir results/$1
+    python $2 0 1000 $1 $3 $4 $5 $6 $7 | tee results/$1/log.txt
+}
+# $1 is experiment name
+# $2 is script name
+# $3 is dataset name
+# $4 is random drop
+# $5 is pretrain
+# $6 is limited_data
+
+run_experiment "MSSEG_pretrained_limited" train_progressive.py MSSEG 0 0 1 500
+run_experiment "MSSEG_pretrained_all" train_progressive.py MSSEG 0 0 0 500
